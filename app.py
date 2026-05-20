46dbed7e83a4fcf9cbe782da5b76bf74bb151769
Psalmist70 <93396284+Psalmist70@users.noreply.github.com>
2026-05-20 20:52:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # LOAD MODEL & ARTIFACTS
 # =========================
 
-model = load_model("models/sms_phishing_cnn_lstm_model.h5")
+model = load_model("models/sms_phishing_cnn_lstm_model.h5", compile=False)
 tokenizer = joblib.load("artifacts/tokenizer.joblib")
 max_len = joblib.load("artifacts/max_len.joblib")
 
